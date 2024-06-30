3ed5175e7756a0fb8010dc01d284724155b61cbb
tkanotmio <zhixianggaoqiao97@gmail.com>
2024-06-30 13:54:35 +0900

    HoT: initial revision

---
+++ HeadorTails.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Tossing a coin...\n");
+    int count[2];
+    for(int i=0; i<2; i++){
+        count[i]=0;
+    }
+    for(int i=0; i<3; i++){
+        printf("Round %d: ", i+1);
+        int num = rand()%2;
+        if(num==0){
+            printf("Heads\n");
+            count[0]++;
+        }
+        else{
+            printf("Tails\n");
+            count[1]++;
+        }
+    }
+    printf("Heads %d, Tails: %d\n", count[0], count[1]);
+    return 0;
+}
